e64452ac8a9225017688808fa874a00181c6e7ff
cloudred <cloudred442@gmail.com>
2025-01-04 17:02:57 +0800

    fix entrypoint.sh

--- scripts/entrypoint_overwrited.sh
+++ scripts/entrypoint_overwrited.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 # Export all secrets in /run/secrets as environment variables
+for secret_file in /run/secrets/*; do
+  if [ -f "$secret_file" ]; then
+    secret_name=$(basename "$secret_file")
+    secret_value=$(cat "$secret_file")
+    export "$secret_name=$secret_value"
+  fi
+done
+
+# Execute the default command
+exec "$@"
+#!/bin/bash
+
+# Export all secrets in /run/secrets as environment variables
 if [ -d "/run/secrets" ]; then
   export $(grep -v '^#' /run/secrets/* | xargs)
 fi
