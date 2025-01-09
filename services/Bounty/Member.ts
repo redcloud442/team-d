c17991e1ef5d8e9eb8be4f251a941823c831a03a
cloudred <cloudred442@gmail.com>
2025-01-09 10:33:25 +0800

    newest commit

--- services/Bounty/Member.ts
+++ services/Bounty/Member.ts
@@ -2,22 +2,33 @@ import { LegionRequestData } from "@/utils/types";
 import { user_table } from "@prisma/client";
 import { SupabaseClient } from "@supabase/supabase-js";
 
-export const getAllyBounty = async (
-  supabaseClient: SupabaseClient,
-  params: {
-    page: number;
-    limit: number;
-    search?: string;
-    teamMemberId: string;
-    columnAccessor: string;
-    isAscendingSort: boolean;
-  }
-) => {
-  const { data, error } = await supabaseClient.rpc("get_ally_bounty", {
-    input_data: params,
-  });
+export const getAllyBounty = async (params: {
+  page: number;
+  limit: number;
+  search?: string;
+  columnAccessor: string;
+  isAscendingSort: boolean;
+}) => {
+  const urlParams = {
+    page: params.page.toString(),
+    limit: params.limit.toString(),
+    search: params.search || "",
+    columnAccessor: params.columnAccessor,
+    isAscendingSort: params.isAscendingSort.toString(),
+  };
 
-  if (error) throw error;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/referrals/direct?${new URLSearchParams(urlParams)}`,
+    {
+      method: "GET",
+    }
+  );
+
+  const result = await response.json();
+
+  if (!response.ok) throw new Error(result.error);
+
+  const { data } = result;
 
   return data as {
     data: (user_table & { total_bounty_earnings: string })[];
