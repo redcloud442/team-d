370eb1e6425e5b4aa19e370daa22376097901727
cloudred <cloudred442@gmail.com>
2025-04-11 19:23:00 +0800

    newest commit

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -14,66 +14,67 @@ const Page = async () => {
     redirect("/admin");
   }
 
-  const [packages, testimonials, wheel, reinvestment, raffle] =
-    await Promise.all([
-      prisma.package_table.findMany({
-        where: { package_is_disabled: false },
-        select: {
-          package_id: true,
-          package_name: true,
-          package_percentage: true,
-          packages_days: true,
-          package_description: true,
-          package_color: true,
-          package_is_disabled: true,
-          package_image: true,
-        },
-      }),
-      prisma.alliance_testimonial_table.findMany({
-        where: { alliance_testimonial_is_hidden: false },
-        select: {
-          alliance_testimonial_id: true,
-          alliance_testimonial_date_created: true,
-          alliance_testimonial_url: true,
-          alliance_testimonial_thumbnail: true,
-          alliance_testimonial_is_hidden: true,
-        },
-        orderBy: {
-          alliance_testimonial_date_created: "desc",
-        },
-      }),
-      prisma.alliance_wheel_settings_table.findMany({
-        orderBy: {
-          alliance_wheel_settings_date: "desc",
-        },
-        select: {
-          alliance_wheel_settings_id: true,
-          alliance_wheel_settings_label: true,
-          alliance_wheel_settings_percentage: true,
-          alliance_wheel_settings_color: true,
-        },
-      }),
-      prisma.package_table.findMany({
-        select: {
-          package_id: true,
-          package_percentage: true,
-          package_is_disabled: true,
-          package_name: true,
-          package_description: true,
-          packages_days: true,
-          package_color: true,
-          package_image: true,
-        },
-      }),
-      prisma.alliance_promo_banner_table.findMany({
-        where: {
-          alliance_promo_banner_is_disabled: false,
-        },
-        orderBy: {
-          alliance_promo_banner_date: "desc",
-        },
-      }),
-    ]);
+  const packages = await prisma.package_table.findMany({
+    where: { package_is_disabled: false },
+    select: {
+      package_id: true,
+      package_name: true,
+      package_percentage: true,
+      packages_days: true,
+      package_description: true,
+      package_color: true,
+      package_is_disabled: true,
+      package_image: true,
+    },
+  });
+
+  const testimonials = await prisma.alliance_testimonial_table.findMany({
+    where: { alliance_testimonial_is_hidden: false },
+    select: {
+      alliance_testimonial_id: true,
+      alliance_testimonial_date_created: true,
+      alliance_testimonial_url: true,
+      alliance_testimonial_thumbnail: true,
+      alliance_testimonial_is_hidden: true,
+    },
+    orderBy: {
+      alliance_testimonial_date_created: "desc",
+    },
+  });
+
+  const wheel = await prisma.alliance_wheel_settings_table.findMany({
+    orderBy: {
+      alliance_wheel_settings_date: "desc",
+    },
+    select: {
+      alliance_wheel_settings_id: true,
+      alliance_wheel_settings_label: true,
+      alliance_wheel_settings_percentage: true,
+      alliance_wheel_settings_color: true,
+    },
+  });
+
+  const reinvestment = await prisma.package_table.findMany({
+    select: {
+      package_id: true,
+      package_percentage: true,
+      package_is_disabled: true,
+      package_name: true,
+      package_description: true,
+      packages_days: true,
+      package_color: true,
+      package_image: true,
+    },
+  });
+
+  const raffle = await prisma.alliance_promo_banner_table.findMany({
+    where: {
+      alliance_promo_banner_is_disabled: false,
+    },
+    orderBy: {
+      alliance_promo_banner_date: "desc",
+    },
+  });
 
   return (
     <DashboardPage
