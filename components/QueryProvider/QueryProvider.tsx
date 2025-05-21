c916c959661ad92112daa39038a2a0510c50403a
cloudred <cloudred442@gmail.com>
2025-05-21 14:25:50 +0800

    newest commit"

--- components/QueryProvider/QueryProvider.tsx
+++ components/QueryProvider/QueryProvider.tsx
@@ -1,12 +1,12 @@
-"use client";
+// "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode } from "react";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { ReactNode } from "react";
 
-export default function QueryProvider({ children }: { children: ReactNode }) {
-  const queryClient = new QueryClient();
+// export default function QueryProvider({ children }: { children: ReactNode }) {
+//   const queryClient = new QueryClient();
 
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
-}
+//   return (
+//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+//   );
+// }
