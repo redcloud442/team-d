64e399e494f45e138dedb3b285a6e7dc0a74b026
cloudred <cloudred442@gmail.com>
2025-05-29 13:39:22 +0800

    newest commit

--- next.config.ts
+++ next.config.ts
@@ -19,7 +19,7 @@ const nextConfig = {
     return [
       {
         source: "/api/v1/:path*",
-        destination: `${process.env.NODE_ENV === "development" ? "http://localhost:3000" : "http://localhost:8000"}/api/v1/:path*`,
+        destination: `${process.env.NODE_ENV === "development" ? "http://localhost:3000" : process.env.API_URL}/api/v1/:path*`,
       },
     ];
   },
