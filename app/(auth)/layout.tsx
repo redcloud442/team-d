cf45b9c53e4578d07f829c2e3d7950462404d951
cloudred <cloudred442@gmail.com>
2025-06-02 13:57:47 +0800

    newest commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <section className="min-h-screen h-full flex items-center justify-center sm:p-0 p-0">
-      <div className="absolute top-0 left-0 w-full h-full bg-bg-primary z-10" />
+    <section className="min-h-screen h-full flex items-center justify-center sm:p-0 p-0 overflow-hidden">
+      <div className="absolute top-0 left-0 w-full h-full bg-bg-primary z-10 overflow-hidden" />
 
       {children}
     </section>
